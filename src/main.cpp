5fe91ffba00b79f0137a72b605dbe4f944cd507f
Michael Gerhold <michael.gerhold@web.de>
2022-09-27 17:16:20 +0200

    implemented ability to enable or disable optimizations with a command line flag

--- src/main.cpp
+++ src/main.cpp
@@ -272,6 +272,7 @@ int main(int, char** argv) {
                     .optionally_named<
                             'l', "lib", "pass a comma-separated list of paths to use for import-lookup", std::string>(""
                     )
+                    .flag<'O', "optimize", "enable the optimizer">()
                     .flag<'v', "verbose", "show verbose output">()
                     .create();
 
@@ -354,15 +355,21 @@ int main(int, char** argv) {
         bssembly += std::visit(Emitter::Emitter{ &program, &label_generator, &type_container }, item);
     }
 
-    usize size_before = bssembly.size();
-    optimize(bssembly, command_line_parser.get<'v'>());
-    usize size_after = bssembly.size();
-    if (command_line_parser.get<'v'>()) {
-        fmt::print(
-                stderr, "optimization: {:.2} % ({} instructions to {} instructions)\n",
-                static_cast<double>(size_before - size_after) / static_cast<double>(size_before) * 100.0, size_before,
-                size_after, bssembly.size()
-        );
+    if (command_line_parser.get<'O'>()) {
+        const auto size_before = bssembly.size();
+
+        optimize(bssembly, command_line_parser.get<'v'>());
+
+        const auto size_after = bssembly.size();
+        if (command_line_parser.get<'v'>()) {
+            fmt::print(
+                    stderr, "optimization: {:.2} % ({} instructions to {} instructions)\n",
+                    static_cast<double>(size_before - size_after) / static_cast<double>(size_before) * 100.0,
+                    size_before, size_after, bssembly.size()
+            );
+        }
+    } else if (command_line_parser.get<'v'>()) {
+        fmt::print(stderr, "optimizations were disabled");
     }
 
     if (command_line_parser.was_provided<'o'>()) {
